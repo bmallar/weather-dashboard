a31502b10a80fb4ba244fcadaef818017d9788e3
bmallar <benmallar@gmail.com>
2024-03-12 16:06:32 -0500

    got local storage working added a header and css for it

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,36 @@
-const getWeather = function () {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=key`
+const searchInputEl = document.querySelector('#search');
+const searchBtnEl = document.querySelector('#search-btn');
+
+
+const searchHandler = function (event) {
+    event.preventDefault();
+
+    const searcher = searchInputEl.value.trim();
+
+    if (searcher) {
+        getWeather(searcher);
+
+        searchInputEl.value = '';
+    } else {
+        alert(`please enter a City`);
+    }
+
+
+}
+
+
+const getWeather = function (city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9898dc8d721936b2fb56457b35b82a47`
 
     fetch(apiUrl)
     .then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
                 console.log(data);
-                displayWeather(data.items)
+                displayWeather(data, city);
             })
         }
     })
 }
+
+searchBtnEl.addEventListener('click', searchHandler);
